3024fd47d07a36b1e2cc98e6307171f7c2ccd868
Hlova Oleh <hlovaoleh0@gmail.com>
2024-01-26 21:23:48 +0200

    Implemented docker into service api

--- airport_service/settings.py
+++ airport_service/settings.py
@@ -88,8 +88,11 @@ WSGI_APPLICATION = "airport_service.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "HOST": os.environ["POSTGRES_HOST"],
+        "NAME": os.environ["POSTGRES_DB"],
+        "USER": os.environ["POSTGRES_USER"],
+        "PASSWORD": os.environ["POSTGRES_PASSWORD"],
     }
 }
 
@@ -157,7 +160,7 @@ SIMPLE_JWT = {
 
 MEDIA_URL = "/media/"
 
-MEDIA_ROOT = BASE_DIR / "media"
+MEDIA_ROOT = "/vol/web/media"
 
 SPECTACULAR_SETTINGS = {
     "TITLE": "Airport Service API",
